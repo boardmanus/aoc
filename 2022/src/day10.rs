29153b31955b7c81cd898eabff29a4d9939dd844
Craig <boardmanzuz@gmail.com>
2022-12-11 08:03:57 +1300

    day 10 part 1

--- 2022/src/day10.rs
+++ 2022/src/day10.rs
@@ -1,15 +1,66 @@
 use crate::aoc::Aoc;
 
+enum Op {
+    Noop,
+    Addx(i32),
+}
+
+fn signal_strength(cycle: i32, x: i32) -> i32 {
+    cycle * x
+}
+
+fn line_to_op(line: &str) -> Op {
+    let mut op_strs = line.split(' ');
+    match op_strs.next().unwrap() {
+        "noop" => Op::Noop,
+        "addx" => Op::Addx(op_strs.next().unwrap().parse::<i32>().unwrap()),
+        _ => panic!(),
+    }
+}
+
 pub struct Day10_1;
 impl Aoc for Day10_1 {
     fn day(&self) -> u32 {
         10
     }
     fn puzzle_name(&self) -> &str {
-        "???"
+        "CRT"
     }
     fn solve(&self, lines: &Vec<String>) -> String {
-        lines[0].clone()
+        //const SAMPLE_CYCLES: [i32; 6] = [20, 60, 100, 140, 180, 220];
+        let mut sample_num = 20;
+        let mut x = 1;
+        let mut cycle: i32 = 0;
+        lines
+            .iter()
+            .map(|line| line_to_op(line))
+            .fold(0, |sum, op| -> i32 {
+                let old_x = x;
+                x += match op {
+                    Op::Noop => {
+                        cycle += 1;
+                        0
+                    }
+                    Op::Addx(dx) => {
+                        cycle += 2;
+                        dx
+                    }
+                };
+                println!("sample_num={sample_num}, cycle={cycle}, old_x={old_x}, x={x}, sum={sum}");
+                let old_sample_num = sample_num;
+                if cycle > sample_num {
+                    sample_num += 40;
+                    println!("signal strength={}", old_sample_num * old_x);
+                    sum + old_sample_num * old_x
+                } else if cycle == sample_num {
+                    sample_num += 40;
+                    println!("signal strength={}", cycle * x);
+                    sum + cycle * old_x
+                } else {
+                    sum
+                }
+            })
+            .to_string()
     }
 }
 
@@ -19,10 +70,42 @@ impl Aoc for Day10_2 {
         10
     }
     fn puzzle_name(&self) -> &str {
-        "??? 2"
+        "CRT 2"
     }
     fn solve(&self, lines: &Vec<String>) -> String {
-        lines[0].clone()
+        let mut sample_num = 20;
+        let mut x = 1;
+        let mut cycle: i32 = 0;
+        lines
+            .iter()
+            .map(|line| line_to_op(line))
+            .fold(0, |sum, op| -> i32 {
+                let old_x = x;
+                x += match op {
+                    Op::Noop => {
+                        cycle += 1;
+                        0
+                    }
+                    Op::Addx(dx) => {
+                        cycle += 2;
+                        dx
+                    }
+                };
+                println!("sample_num={sample_num}, cycle={cycle}, old_x={old_x}, x={x}, sum={sum}");
+                let old_sample_num = sample_num;
+                if cycle > sample_num {
+                    sample_num += 40;
+                    println!("signal strength={}", old_sample_num * old_x);
+                    sum + old_sample_num * old_x
+                } else if cycle == sample_num {
+                    sample_num += 40;
+                    println!("signal strength={}", cycle * x);
+                    sum + cycle * old_x
+                } else {
+                    sum
+                }
+            })
+            .to_string()
     }
 }
 
@@ -66,6 +149,15 @@ mod tests {
             .into_iter()
             .collect::<Vec<String>>();
 
-        assert_eq!(Day10_2.solve(&input_strs), 36.to_string());
+        assert_eq!(
+            Day10_2.solve(&input_strs),
+            "##..##..##..##..##..##..##..##..##..##..
+###...###...###...###...###...###...###.
+####....####....####....####....####....
+#####.....#####.....#####.....#####.....
+######......######......######......####
+#######.......#######.......#######....."
+                .to_string()
+        );
     }
 }
