e1ccae12b90e40ac2d4a1c23ba25114c4c342519
Craig Boardman <craig_boardman@trimble.com>
2023-01-03 16:07:01 +1300

    Refactor day22 a bit for the cube ...

--- 2022/day22/src/main.rs
+++ 2022/day22/src/main.rs
@@ -1,48 +1,66 @@
-use std::fmt::Display;
+use std::{collections::HashMap, fmt::Display};
 
 use regex::Regex;
 
 #[derive(Debug, PartialEq, Clone)]
-struct Row {
-    start: usize,
-    ground: Vec<bool>,
-}
+struct Row(Vec<Option<bool>>);
 
 impl Display for Row {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        for _ in 0..self.start {
-            write!(f, " ")?;
-        }
-        for b in &self.ground {
-            write!(f, "{}", if *b { '.' } else { '#' })?;
+        for b in &self.0 {
+            write!(
+                f,
+                "{}",
+                if let Some(c) = *b {
+                    if c {
+                        '.'
+                    } else {
+                        '#'
+                    }
+                } else {
+                    ' '
+                }
+            )?;
         }
         Ok(())
     }
 }
 
 impl Row {
-    fn new(start: usize, ground: &[bool]) -> Row {
-        Row {
-            start,
-            ground: ground.into(),
+    fn parse(line: &str, width: usize) -> Row {
+        let mut row = Vec::<Option<bool>>::with_capacity(width);
+        for c in line.chars() {
+            row.push(match c {
+                '.' => Some(true),
+                '#' => Some(false),
+                _ => None,
+            })
         }
+        for _ in row.len()..width {
+            row.push(None);
+        }
+        Row(row)
     }
+}
 
-    fn parse(line: &str) -> Row {
-        let start = line.find(&['.', '#']).unwrap();
-        let ground_str = &line[start..line.len()];
-        let mut ground = Vec::<bool>::with_capacity(ground_str.len());
-        for (i, c) in ground_str.chars().enumerate() {
-            ground.push(c == '.');
+#[derive(Debug, Clone, Copy, PartialEq)]
+enum Rotate {
+    Left,
+    Right,
+}
+
+impl Rotate {
+    fn val(&self) -> i64 {
+        match self {
+            Rotate::Left => -1,
+            Rotate::Right => 1,
         }
-        Row { start, ground }
     }
 }
 
 #[derive(Debug, Clone, Copy, PartialEq)]
 enum Cmd {
-    Left,
-    Right,
+    Rot(Rotate),
     Fwd(usize),
 }
 
@@ -51,8 +69,8 @@ impl Cmd {
         let re = Regex::new(r"L|R|\d+").unwrap();
         line.split_inclusive(&re)
             .flat_map(|x| match x {
-                "L" => Some(Cmd::Left),
-                "R" => Some(Cmd::Right),
+                "L" => Some(Cmd::Rot(Rotate::Left)),
+                "R" => Some(Cmd::Rot(Rotate::Right)),
                 "" => None,
                 _ => Some(Cmd::Fwd(x.parse().unwrap())),
             })
@@ -60,11 +78,13 @@ impl Cmd {
     }
 }
 
-type Col = Row;
 struct Map {
+    cubic: bool,
+    sqr_size: usize,
     rows: Vec<Row>,
-    cols: Vec<Col>,
 }
+
+type Path = HashMap<(i64, i64), Dir>;
 
 impl Display for Map {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
@@ -72,123 +92,167 @@ impl Display for Map {
             writeln!(f, "{row}")?;
         }
         writeln!(f)?;
-        for col in &self.cols {
-            writeln!(f, "{col}")?;
-        }
         Ok(())
     }
 }
 impl Map {
+    fn print_path(&self, path: &Path) {
+        for y in 0..self.rows.len() {
+            for x in 0..self.rows[0].0.len() {
+                match self.rows[y].0[x] {
+                    None => print!(" "),
+                    Some(false) => print!("#"),
+                    Some(true) => {
+                        if let Some(step) = path.get(&(x as i64, y as i64)) {
+                            print!("{step}")
+                        } else {
+                            print!(".");
+                        }
+                    }
+                }
+            }
+            println!();
+        }
+        println!();
+    }
+
     fn start_pos(&self) -> Pos {
         let row = &self.rows[0];
-        let i = row.ground.iter().enumerate().find(|x| *x.1).unwrap();
+        let i = row
+            .0
+            .iter()
+            .enumerate()
+            .find(|x| x.1.unwrap_or(false))
+            .unwrap();
         Pos {
-            x: (i.0 + row.start) as i64,
+            x: i.0 as i64,
             y: 0,
             dir: Dir::Right,
         }
     }
 
     fn next(&self, pos: &Pos) -> Option<Pos> {
+        let dir = pos.dir.dir();
+        let x = (pos.x + dir.0).rem_euclid(self.rows[0].0.len() as i64);
+        let y = (pos.y + dir.1).rem_euclid(self.rows.len() as i64);
+        if let Some(g) = self.rows[y as usize].0[x as usize] {
+            if g {
+                Some(Pos { x, y, dir: pos.dir })
+            } else {
+                None
+            }
+        } else {
+            self.teleport(pos)
+        }
+    }
+
+    fn cell_pos(&self, old_pos: &Pos, new_pos: &Pos) -> Option<Pos> {
+        let cell = self.rows[new_pos.y as usize].0[new_pos.x as usize];
+        if let Some(c) = cell {
+            if c {
+                Some(*new_pos)
+            } else {
+                Some(*old_pos)
+            }
+        } else {
+            None
+        }
+    }
+
+    fn teleport_cubic(&self, pos: &Pos) -> Option<Pos> {
+        None
+    }
+    fn teleport_flat(&self, pos: &Pos) -> Option<Pos> {
         match pos.dir {
-            Dir::Right => {
-                let row = &self.rows[pos.y as usize];
-                let mut x = pos.x + 1;
-                if x as usize >= row.start + row.ground.len() {
-                    x = row.start as i64;
-                }
-                if row.ground[x as usize - row.start] {
-                    Some(Pos {
-                        x,
-                        y: pos.y,
-                        dir: pos.dir,
-                    })
-                } else {
-                    None
-                }
-            }
-            Dir::Down => {
-                let col = &self.cols[pos.x as usize];
-                let mut y = pos.y + 1;
-                if y as usize >= col.start + col.ground.len() {
-                    y = col.start as i64;
-                }
-                if col.ground[y as usize - col.start] {
-                    Some(Pos {
-                        x: pos.x,
-                        y,
-                        dir: pos.dir,
-                    })
-                } else {
-                    None
-                }
-            }
-            Dir::Left => {
-                let row = &self.rows[pos.y as usize];
-                let mut x = pos.x - 1;
-                if x < row.start as i64 {
-                    x = (row.start + row.ground.len() - 1) as i64;
-                }
-                if row.ground[x as usize - row.start] {
-                    Some(Pos {
-                        x,
-                        y: pos.y,
-                        dir: pos.dir,
-                    })
-                } else {
-                    None
-                }
-            }
-            Dir::Up => {
-                let col = &self.cols[pos.x as usize];
-                let mut y = pos.y - 1;
-                if y < col.start as i64 {
-                    y = (col.start + col.ground.len() - 1) as i64;
-                }
-                if col.ground[y as usize - col.start] {
-                    Some(Pos {
-                        x: pos.x,
-                        y,
-                        dir: pos.dir,
-                    })
-                } else {
-                    None
-                }
-            }
+            Dir::Right => (0..(self.rows[0].0.len() / self.sqr_size)).find_map(|i| {
+                self.cell_pos(pos, &Pos::new((i * self.sqr_size) as i64, pos.y, pos.dir))
+            }),
+            Dir::Down => (0..(self.rows.len() / self.sqr_size)).find_map(|i| {
+                self.cell_pos(pos, &Pos::new(pos.x, (i * self.sqr_size) as i64, pos.dir))
+            }),
+            Dir::Left => (1..(self.rows[0].0.len() / self.sqr_size + 1))
+                .rev()
+                .find_map(|i| {
+                    self.cell_pos(
+                        pos,
+                        &Pos::new((i * self.sqr_size - 1) as i64, pos.y, pos.dir),
+                    )
+                }),
+            Dir::Up => (1..(self.rows.len() / self.sqr_size + 1))
+                .rev()
+                .find_map(|i| {
+                    self.cell_pos(
+                        pos,
+                        &Pos::new(pos.x, (i * self.sqr_size - 1) as i64, pos.dir),
+                    )
+                }),
+        }
+    }
+    fn teleport(&self, pos: &Pos) -> Option<Pos> {
+        if self.cubic {
+            self.teleport_cubic(pos)
+        } else {
+            self.teleport_flat(pos)
         }
     }
 
-    fn cols_from_rows(rows: &Vec<Row>) -> Vec<Col> {
-        let mut cols = Vec::<Col>::default();
-
-        for row in rows.iter().enumerate() {
-            for col_idx in 0..(row.1.start + row.1.ground.len()) {
-                if let Some(col) = cols.get_mut(col_idx) {
-                    if col_idx < row.1.start {
-                        col.start += 1;
-                    } else {
-                        assert_eq!(col.start + col.ground.len(), row.0);
-                        col.ground.push(row.1.ground[col_idx - row.1.start]);
-                    }
-                } else {
-                    let mut col = Col::new(row.0, Default::default());
-                    if col_idx < row.1.start {
-                        col.start += 1;
-                    } else {
-                        assert_eq!(col.start + col.ground.len(), row.0);
-                        col.ground.push(row.1.ground[col_idx - row.1.start]);
-                    }
-                    cols.push(col);
-                }
-            }
+    fn parse(input: &str, cubic: bool) -> Map {
+        let lines = input.split('\n');
+        let width = lines.clone().map(|l| l.len()).max().unwrap();
+        let rows = lines.map(|s| Row::parse(s, width)).collect::<Vec<_>>();
+        let sqr_size = if rows.len() / 50 > 0 && rows[0].0.len() > 50 {
+            50
+        } else {
+            4
+        };
+        Map {
+            cubic,
+            sqr_size,
+            rows,
         }
-        cols
     }
 
-    fn parse(input: &str) -> Map {
-        let rows = input.split('\n').map(|s| Row::parse(s)).collect();
-        let cols = Map::cols_from_rows(&rows);
-        Map { rows, cols }
+    fn operate(&self, pos: &Pos, cmd: Cmd, steps: &mut Path) -> Pos {
+        let next_pos = match cmd {
+            Cmd::Rot(dir) => Pos {
+                x: pos.x,
+                y: pos.y,
+                dir: pos.dir.rotate(dir),
+            },
+            Cmd::Fwd(dist) => {
+                let mut new_pos = *pos;
+                for _ in 0..dist {
+                    if let Some(p) = self.next(&new_pos) {
+                        steps.insert((p.x, p.y), p.dir);
+                        new_pos = p;
+                    } else {
+                        break;
+                    }
+                }
+                new_pos
+            }
+        };
+        steps.insert((next_pos.x, next_pos.y), next_pos.dir);
+        next_pos
+    }
+
+    fn walk(&self, cmds: &[Cmd]) -> (Pos, Path) {
+        let mut path = Path::default();
+        let start_pos = self.start_pos();
+        path.insert((start_pos.x, start_pos.y), start_pos.dir);
+        let pos = cmds.iter().fold(start_pos, |pos, cmd| match *cmd {
+            Cmd::Rot(dir) => {
+                let p = Pos {
+                    x: pos.x,
+                    y: pos.y,
+                    dir: pos.dir.rotate(dir),
+                };
+                path.insert((p.x, p.y), p.dir);
+                p
+            }
+            Cmd::Fwd(_) => self.operate(&pos, *cmd, &mut path),
+        });
+        (pos, path)
     }
 }
 
@@ -199,6 +263,46 @@ enum Dir {
     Left,
     Up,
 }
+
+impl From<i64> for Dir {
+    fn from(value: i64) -> Self {
+        match value {
+            0 => Dir::Right,
+            1 => Dir::Down,
+            2 => Dir::Left,
+            3 => Dir::Up,
+            _ => panic!(),
+        }
+    }
+}
+impl Dir {
+    fn dir(&self) -> (i64, i64) {
+        match self {
+            Dir::Right => (1, 0),
+            Dir::Down => (0, 1),
+            Dir::Left => (-1, 0),
+            Dir::Up => (0, -1),
+        }
+    }
+
+    fn rotate(&self, dir: Rotate) -> Dir {
+        (dir.val() + *self as i64).rem_euclid(4).into()
+    }
+}
+
+impl Display for Dir {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let c = match self {
+            Dir::Right => '>',
+            Dir::Down => 'v',
+            Dir::Left => '<',
+            Dir::Up => '^',
+        };
+        write!(f, "{c}")
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
 struct Pos {
     x: i64,
     y: i64,
@@ -206,78 +310,36 @@ struct Pos {
 }
 
 impl Pos {
+    fn new(x: i64, y: i64, dir: Dir) -> Pos {
+        Pos { x, y, dir }
+    }
+
     fn password(&self) -> usize {
         (self.y as usize + 1) * 1000 + (self.x as usize + 1) * 4 + self.dir as usize
     }
 }
 
-fn parse(input: &str) -> (Map, Vec<Cmd>) {
+fn parse(input: &str, cubic: bool) -> (Map, Vec<Cmd>) {
     let partition = input.split("\n\n").collect::<Vec<_>>();
     assert_eq!(partition.len(), 2);
-    (Map::parse(partition[0]), Cmd::parse(partition[1].trim()))
+    (
+        Map::parse(partition[0], cubic),
+        Cmd::parse(partition[1].trim()),
+    )
 }
 
-fn new_dir(curr_dir: Dir, cmd: Cmd) -> Dir {
-    match curr_dir {
-        Dir::Right => {
-            if cmd == Cmd::Left {
-                Dir::Up
-            } else {
-                Dir::Down
-            }
-        }
-        Dir::Left => {
-            if cmd == Cmd::Left {
-                Dir::Down
-            } else {
-                Dir::Up
-            }
-        }
-        Dir::Up => {
-            if cmd == Cmd::Left {
-                Dir::Left
-            } else {
-                Dir::Right
-            }
-        }
-        Dir::Down => {
-            if cmd == Cmd::Left {
-                Dir::Right
-            } else {
-                Dir::Left
-            }
-        }
-    }
-}
 fn solve_part1(input: &str) -> String {
-    let (map, cmds) = parse(input);
-    let start = map.start_pos();
-    let pos = cmds.iter().fold(start, |pos, cmd| {
-        let next_pos = match *cmd {
-            Cmd::Left | Cmd::Right => Pos {
-                x: pos.x,
-                y: pos.y,
-                dir: new_dir(pos.dir, *cmd),
-            },
-            Cmd::Fwd(dist) => {
-                let mut new_pos = pos;
-                for _ in 0..dist {
-                    if let Some(p) = map.next(&new_pos) {
-                        new_pos = p;
-                    } else {
-                        break;
-                    }
-                }
-                new_pos
-            }
-        };
-        next_pos
-    });
-    pos.password().to_string()
+    let (map, cmds) = parse(input, false);
+    let (final_pos, path) = map.walk(&cmds);
+    map.print_path(&path);
+    println!("Final Pos: {:?}", final_pos);
+    final_pos.password().to_string()
 }
 
 fn solve_part2(input: &str) -> String {
-    0.to_string()
+    let (map, cmds) = parse(input, true);
+    let (pos, path) = map.walk(&cmds);
+    pos.password().to_string()
 }
 
 fn main() {
@@ -302,14 +364,27 @@ mod tests {
 
     #[test]
     fn test_part2() {
-        assert_eq!(solve_part2(TEST_INPUT), 0.to_string());
+        assert_eq!(solve_part2(TEST_INPUT), 5031.to_string());
     }
 
     #[test]
     fn test_parse_line() {
         assert_eq!(
-            Row::parse("     ...#..."),
-            Row::new(5, &[true, true, true, false, true, true, true])
+            Row::parse("     ...#...", 12),
+            Row(vec![
+                None,
+                None,
+                None,
+                None,
+                None,
+                Some(true),
+                Some(true),
+                Some(true),
+                Some(false),
+                Some(true),
+                Some(true),
+                Some(true)
+            ])
         );
     }
 
@@ -319,17 +394,17 @@ mod tests {
             Cmd::parse("10R5L5R10L4R5L5"),
             vec![
                 Cmd::Fwd(10),
-                Cmd::Right,
+                Cmd::Rot(Rotate::Right),
                 Cmd::Fwd(5),
-                Cmd::Left,
+                Cmd::Rot(Rotate::Left),
                 Cmd::Fwd(5),
-                Cmd::Right,
+                Cmd::Rot(Rotate::Right),
                 Cmd::Fwd(10),
-                Cmd::Left,
+                Cmd::Rot(Rotate::Left),
                 Cmd::Fwd(4),
-                Cmd::Right,
+                Cmd::Rot(Rotate::Right),
                 Cmd::Fwd(5),
-                Cmd::Left,
+                Cmd::Rot(Rotate::Left),
                 Cmd::Fwd(5)
             ]
         )
@@ -337,10 +412,9 @@ mod tests {
 
     #[test]
     fn test_parse_input() {
-        let res = parse(TEST_INPUT);
+        let res = parse(TEST_INPUT, false);
         println!("{}", res.0);
         assert_eq!(res.0.rows.len(), 12);
-        assert_eq!(res.0.cols.len(), 12);
         assert_eq!(res.1.len(), 13);
     }
 }
