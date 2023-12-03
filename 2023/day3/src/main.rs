2b3a5d1f7f4c0175247f41eb84bb3c58f5d6b7a3
Craig Boardman <craig_boardman@trimble.com>
2023-12-03 13:00:30 +1300

    Add day3 part 2... not working properly yet :|

--- 2023/day3/src/main.rs
+++ 2023/day3/src/main.rs
@@ -1,3 +1,14 @@
+use std::collections::{HashMap, HashSet};
+
+#[derive(Debug, PartialEq, Eq, Hash)]
+enum Cell {
+    Empty,
+    Part(u64, (i64, i64)),
+    Gear,
+}
+
+type EngineMap = HashMap<(i64, i64), Cell>;
+
 #[derive(Debug)]
 struct EnginePart {
     num: usize,
@@ -9,6 +20,42 @@ struct EnginePart {
 struct Grid {
     grid: Vec<Vec<char>>,
     parts: Vec<EnginePart>,
+}
+
+fn fill_engine_map(mut em: EngineMap, line: (usize, &&str)) -> EngineMap {
+    let mut num_str: &str = line.1;
+    let mut id = (0i64, 0i64);
+    let mut q: Vec<(i64, i64)> = Vec::new();
+    num_str.chars().enumerate().for_each(|(i, c)| match c {
+        '0'..='9' => {
+            let pos = (i as i64, line.0 as i64);
+            if q.len() == 0 {
+                num_str = &line.1[i..];
+                id = pos;
+            }
+            q.push(pos);
+        }
+        '*' => {
+            if q.len() != 0 {
+                let num = num_str[..q.len()].parse::<u64>().unwrap();
+                q.iter().for_each(|(x, y)| {
+                    em.insert((*x, *y), Cell::Part(num as u64, id));
+                });
+                q.clear();
+            }
+            em.insert((i as i64, line.0 as i64), Cell::Gear);
+        }
+        _ => {
+            if q.len() != 0 {
+                let num = num_str[..q.len()].parse::<u64>().unwrap();
+                q.iter().for_each(|(x, y)| {
+                    em.insert((*x, *y), Cell::Part(num as u64, id));
+                });
+                q.clear();
+            }
+        }
+    });
+    em
 }
 
 fn line_engine_parts(line: &str, row: usize) -> Vec<EnginePart> {
@@ -90,8 +137,49 @@ fn solve_part1(input: &str) -> usize {
         })
 }
 
-fn solve_part2(input: &str) -> usize {
-    0
+const GRID: [(i64, i64); 8] = [
+    (-1, -1),
+    (0, -1),
+    (1, -1),
+    (-1, 0),
+    (1, 0),
+    (-1, 1),
+    (0, 1),
+    (1, 1),
+];
+
+fn solve_part2(input: &str) -> u64 {
+    let lines = input.lines().collect::<Vec<_>>();
+
+    let ep = lines
+        .iter()
+        .enumerate()
+        .fold(EngineMap::new(), fill_engine_map);
+
+    ep.iter()
+        .filter(|(_, v)| **v == Cell::Gear)
+        .map(|(k, _)| {
+            let parts = GRID.iter().fold(HashSet::new(), |mut acc, (dx, dy)| {
+                if let Some(Cell::Part(num, id)) = ep.get(&(k.0 + dx, k.1 + dy)) {
+                    acc.insert((*num, *id));
+                }
+                acc
+            });
+            if parts.len() == 2 {
+                let p = parts.iter().map(|v| v.0).product();
+                println!(
+                    "Found 2 parts around ({}, {}) => {:?} => {}",
+                    k.0, k.1, parts, p
+                );
+                p
+            } else {
+                if parts.len() > 0 {
+                    println!("Found {} parts around ({}, {})", parts.len(), k.0, k.1);
+                }
+                0
+            }
+        })
+        .sum()
 }
 
 fn main() {
@@ -107,7 +195,7 @@ mod tests {
     use super::*;
 
     const TEST_INPUT: &str = include_str!("test_input.txt");
-    const TEST_INPUT_2: &str = include_str!("test_input.txt");
+    const TEST_INPUT_2: &str = include_str!("test_input2.txt");
 
     #[test]
     fn test_part1() {
@@ -116,6 +204,11 @@ mod tests {
 
     #[test]
     fn test_part2() {
-        assert_eq!(solve_part2(TEST_INPUT_2), 2286);
+        assert_eq!(solve_part2(TEST_INPUT), 467835);
+    }
+
+    #[test]
+    fn test_part2_2() {
+        assert_eq!(solve_part2(TEST_INPUT_2), 467835);
     }
 }
