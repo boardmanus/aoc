e26ce845f4ffe8b04c30996fdec2b5f215817392
Craig <boardmanzuz@gmail.com>
2023-12-04 22:38:13 +1300

    Implement day4 using nom to parse

--- 2023/day4/src/main.rs
+++ 2023/day4/src/main.rs
@@ -1,72 +1,70 @@
-use lazy_static::lazy_static; // 1.3.0
-use regex::Regex;
 use std::collections::{HashMap, HashSet};
 
-lazy_static! {
-    static ref WHITESPACE_RE: Regex = Regex::new(r"\s+").unwrap();
-    static ref CARD_NUM_RE: Regex = Regex::new(r"Card\s+(\d+)").unwrap();
+use nom::{
+    bytes::complete::tag,
+    character::complete::{space1, u64, u8},
+    multi::separated_list1,
+    sequence::{delimited, pair, preceded, tuple},
+    IResult,
+};
+
+fn card_line(input: &str) -> IResult<&str, (usize, HashSet<u8>, HashSet<u8>)> {
+    let (input, card_num) =
+        delimited(pair(tag("Card"), space1), u64, pair(tag(":"), space1))(input)?;
+    let (input, winning_nums) = separated_list1(space1, u8)(input)?;
+    let (input, card_nums) = preceded(
+        tuple((space1, tag("|"), space1)),
+        separated_list1(space1, u8),
+    )(input)?;
+
+    Ok((
+        input,
+        (
+            card_num as usize,
+            HashSet::from_iter(winning_nums.into_iter()),
+            HashSet::from_iter(card_nums.into_iter()),
+        ),
+    ))
 }
 
-fn parse_line(line: &str) -> (usize, HashSet<u8>, HashSet<u8>) {
-    let mut split = line.split(':');
-    let card_num_str = split.next().unwrap();
+fn scratch_card_sum(line: &str) -> usize {
+    let (_, ticket) = card_line(line).unwrap();
+    let num_matches = ticket.1.intersection(&ticket.2).count();
+    if num_matches == 0 {
+        0
+    } else {
+        1 << (num_matches - 1)
+    }
+}
 
-    let card_num = CARD_NUM_RE
-        .captures(card_num_str)
-        .unwrap()
-        .get(1)
-        .unwrap()
-        .as_str()
-        .parse::<usize>()
-        .unwrap();
+fn scratch_card_copy_sum(
+    mut acc: HashMap<usize, usize>,
+    line: &str,
+    max_card_num: usize,
+) -> HashMap<usize, usize> {
+    let (_, ticket) = card_line(line).unwrap();
+    let card = ticket.0;
+    let num_matches = ticket.1.intersection(&ticket.2).count();
+    *acc.entry(card).or_insert(0) += 1;
 
-    let nums: Vec<HashSet<u8>> = split
-        .next()
-        .unwrap()
-        .trim()
-        .split(" | ")
-        .map(|s| {
-            WHITESPACE_RE
-                .replace_all(s.trim(), " ")
-                .split(' ')
-                .map(|n| n.parse().unwrap())
-                .collect()
-        })
-        .collect();
-    (card_num, nums[0].clone(), nums[1].clone())
+    let num_copies = *acc.get(&card).unwrap();
+    for copy in (card + 1)..max_card_num.min(card + num_matches + 1) {
+        *acc.entry(copy).or_insert(0) += num_copies;
+    }
+
+    acc
 }
 
 fn solve_part1(input: &str) -> usize {
-    input
-        .lines()
-        .map(|line| {
-            let res = parse_line(line);
-            let num_matches = res.1.intersection(&res.2).count();
-            if num_matches == 0 {
-                0
-            } else {
-                1 << (num_matches - 1)
-            }
-        })
-        .sum()
+    input.lines().map(scratch_card_sum).sum()
 }
 
 fn solve_part2(input: &str) -> usize {
-    let lines = input.lines();
-    let max_card_num = lines.count() + 1;
+    let max_card_num = input.lines().count() + 1;
     let card_freqs = input
         .lines()
-        .fold(HashMap::<usize, usize>::default(), |mut acc, line| {
-            let ticket = parse_line(line);
-            let card = ticket.0;
-            let num_matches = ticket.1.intersection(&ticket.2).count();
-            *acc.entry(card).or_insert(0) += 1;
-
-            let num_copies = *acc.get(&card).unwrap();
-            for copy in (card + 1)..max_card_num.min(card + num_matches + 1) {
-                *acc.entry(copy).or_insert(0) += num_copies;
-            }
-            acc
+        .fold(HashMap::<usize, usize>::default(), |acc, line| {
+            scratch_card_copy_sum(acc, line, max_card_num)
         });
     card_freqs.iter().fold(0, |acc, (_, count)| acc + count)
 }
