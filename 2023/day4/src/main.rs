6db500fd6e06709fe9d4e999a6e4f7a76b895782
Craig <boardmanzuz@gmail.com>
2023-12-04 20:57:38 +1300

    Tidy up some day 4 - 2

--- 2023/day4/src/main.rs
+++ 2023/day4/src/main.rs
@@ -1,22 +1,39 @@
+use lazy_static::lazy_static; // 1.3.0
+use regex::Regex;
 use std::collections::{HashMap, HashSet};
 
-fn parse_line(line: &str) -> (HashSet<u8>, HashSet<u8>) {
-    let ws_re = regex::Regex::new(r"\s+").unwrap();
-    let nums: Vec<HashSet<u8>> = line
-        .split(":")
-        .nth(1)
+lazy_static! {
+    static ref WHITESPACE_RE: Regex = Regex::new(r"\s+").unwrap();
+    static ref CARD_NUM_RE: Regex = Regex::new(r"Card\s+(\d+)").unwrap();
+}
+
+fn parse_line(line: &str) -> (usize, HashSet<u8>, HashSet<u8>) {
+    let mut split = line.split(':');
+    let card_num_str = split.next().unwrap();
+
+    let card_num = CARD_NUM_RE
+        .captures(card_num_str)
+        .unwrap()
+        .get(1)
+        .unwrap()
+        .as_str()
+        .parse::<usize>()
+        .unwrap();
+
+    let nums: Vec<HashSet<u8>> = split
+        .next()
         .unwrap()
         .trim()
         .split(" | ")
         .map(|s| {
-            ws_re
+            WHITESPACE_RE
                 .replace_all(s.trim(), " ")
                 .split(' ')
                 .map(|n| n.parse().unwrap())
                 .collect()
         })
         .collect();
-    (nums[0].clone(), nums[1].clone())
+    (card_num, nums[0].clone(), nums[1].clone())
 }
 
 fn solve_part1(input: &str) -> usize {
@@ -24,7 +41,7 @@ fn solve_part1(input: &str) -> usize {
         .lines()
         .map(|line| {
             let res = parse_line(line);
-            let num_matches = res.0.intersection(&res.1).count();
+            let num_matches = res.1.intersection(&res.2).count();
             if num_matches == 0 {
                 0
             } else {
@@ -36,26 +53,22 @@ fn solve_part1(input: &str) -> usize {
 
 fn solve_part2(input: &str) -> usize {
     let lines = input.lines();
-    let max_card = lines.count();
-    let mut card = 0;
+    let max_card_num = lines.count() + 1;
     let card_freqs = input
         .lines()
         .fold(HashMap::<usize, usize>::default(), |mut acc, line| {
             let ticket = parse_line(line);
-            let num_matches = ticket.0.intersection(&ticket.1).count();
+            let card = ticket.0;
+            let num_matches = ticket.1.intersection(&ticket.2).count();
             *acc.entry(card).or_insert(0) += 1;
 
             let num_copies = *acc.get(&card).unwrap();
-            for copy in (card + 1)..max_card.min(card + num_matches + 1) {
+            for copy in (card + 1)..max_card_num.min(card + num_matches + 1) {
                 *acc.entry(copy).or_insert(0) += num_copies;
             }
-            card += 1;
             acc
         });
-    card_freqs.iter().fold(0, |acc, (card, count)| {
-        println!("card={}, count={count}", card + 1);
-        acc + count
-    })
+    card_freqs.iter().fold(0, |acc, (_, count)| acc + count)
 }
 
 fn main() {
@@ -88,6 +101,7 @@ mod tests {
         assert_eq!(
             parse_line("Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1"),
             (
+                3,
                 HashSet::from([1, 21, 53, 59, 44]),
                 HashSet::from([69, 82, 63, 72, 16, 21, 14, 1])
             )
