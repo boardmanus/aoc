22a1c7fb4fa181a35f0efea2e0f6401d3221ef17
Craig <boardmanzuz@gmail.com>
2023-12-17 01:44:52 +1300

    Day 15 part 2

--- 2023/day15/src/main.rs
+++ 2023/day15/src/main.rs
@@ -1,5 +1,69 @@
-fn parse(input: &str) -> Vec<&str> {
+use core::hash;
+use std::collections::LinkedList;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+struct Operation<'a> {
+    label: &'a str,
+    focal_length: Option<usize>,
+}
+
+impl<'a> Operation<'a> {
+    fn new(label: &'a str, focal_length: Option<usize>) -> Self {
+        Self {
+            label,
+            focal_length,
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+struct LensBox<'a> {
+    ops: Vec<Operation<'a>>,
+}
+
+impl<'a> LensBox<'a> {
+    fn new() -> Self {
+        Self { ops: Vec::new() }
+    }
+    fn push(&mut self, op: &Operation<'a>) {
+        match self.ops.iter().position(|inop| inop.label == op.label) {
+            Some(pos) => {
+                self.ops[pos] = *op;
+            }
+            None => self.ops.push(*op),
+        }
+    }
+    fn remove(&mut self, op: &Operation<'a>) {
+        match self.ops.iter().position(|inop| inop.label == op.label) {
+            Some(pos) => {
+                self.ops.remove(pos);
+            }
+            None => (),
+        }
+    }
+}
+fn parse1(input: &str) -> Vec<&str> {
     input.trim().split(',').collect()
+}
+
+fn parse2(input: &str) -> Vec<Operation> {
+    input
+        .trim()
+        .split(',')
+        .map(|op_str| {
+            let chars = op_str.chars();
+            match chars.last().unwrap() {
+                '-' => Operation::new(op_str.split('-').next().unwrap(), None),
+                _ => {
+                    let mut bits = op_str.split('=');
+                    Operation::new(
+                        bits.next().unwrap(),
+                        Some(bits.next().unwrap().parse::<usize>().unwrap()),
+                    )
+                }
+            }
+        })
+        .collect()
 }
 
 fn checksum(steps: &str) -> usize {
@@ -9,11 +73,39 @@ fn checksum(steps: &str) -> usize {
 }
 
 fn solve_part1(input: &str) -> usize {
-    parse(input).iter().map(|x| checksum(x)).sum()
+    parse1(input).iter().map(|x| checksum(x)).sum()
 }
 
-fn solve_part2(input: &str) -> u64 {
-    0
+fn solve_part2(input: &str) -> usize {
+    let ops = parse2(input);
+    let mut boxes: Vec<LensBox<'_>> = Default::default();
+    (0..256).for_each(|_| boxes.push(LensBox::new()));
+
+    ops.iter().enumerate().for_each(|op| {
+        let hash = checksum(op.1.label);
+        let lens_box = &mut boxes[hash];
+        match op.1.focal_length {
+            Some(_) => {
+                lens_box.push(op.1);
+            }
+            None => {
+                lens_box.remove(op.1);
+            }
+        }
+    });
+
+    boxes
+        .iter()
+        .enumerate()
+        .map(|(i, lens_box)| {
+            lens_box
+                .ops
+                .iter()
+                .enumerate()
+                .map(|(j, op)| (i + 1) * (j + 1) * op.focal_length.unwrap() as usize)
+                .sum::<usize>()
+        })
+        .sum()
 }
 
 fn main() {
@@ -39,16 +131,30 @@ mod tests {
 
     #[test]
     fn test_part2() {
-        assert_eq!(solve_part2(TEST_INPUT_2), 467835);
+        assert_eq!(solve_part2(TEST_INPUT_2), 145);
     }
 
     #[test]
     fn test_parse() {
         assert_eq!(
-            parse(TEST_INPUT),
+            parse1(TEST_INPUT),
             vec![
                 "rn=1", "cm-", "qp=3", "cm=2", "qp-", "pc=4", "ot=9", "ab=5", "pc-", "pc=6", "ot=7"
             ]
         );
+    }
+
+    #[test]
+    fn test_parse2() {
+        let p = parse2(TEST_INPUT);
+        println!("{:?}", p);
+    }
+
+    #[test]
+    fn test_sanity() {
+        println!("hash rn = {}", checksum("rn"));
+        println!("hash cm = {}", checksum("cm"));
+        println!("hash qp = {}", checksum("qp"));
+        println!("hash pc = {}", checksum("pc"));
     }
 }
