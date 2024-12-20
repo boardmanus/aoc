84c0a897a93eacf16d493cc0fbf3a7b405538449
Craig <boardmanzuz@gmail.com>
2024-12-21 00:19:14 +1300

    Day20 part 2

--- 2024/day20/src/solution.rs
+++ 2024/day20/src/solution.rs
@@ -59,6 +59,77 @@ impl Maze {
         }
     }
 
+    fn cheats_for_pos(&self, pos: Index, time: i64) -> Vec<Index> {
+        // find all path positions reachable in the time frame
+        let mut cheat_pos: Vec<Index> = vec![];
+        for y in (pos.1 - time)..=(pos.1 + time) {
+            let xmax = time - (y - pos.1).abs();
+            assert!(xmax >= 0 && xmax <= time);
+            for x in (pos.0 - xmax)..=(pos.0 + xmax) {
+                let new_pos = Index(x, y);
+                if new_pos == pos {
+                    continue;
+                }
+                if let Some(c) = self.grid.at(new_pos) {
+                    if c != '#' {
+                        cheat_pos.push(new_pos);
+                    }
+                }
+            }
+        }
+        cheat_pos
+    }
+
+    fn adv_cheat_length(
+        &self,
+        visited: &HashMap<Index, usize>,
+        max_len: usize,
+        start_pos: Index,
+        cheat_pos: Index,
+    ) -> usize {
+        let start_len = visited.get(&start_pos).unwrap();
+        let end_len = max_len - visited.get(&cheat_pos).unwrap();
+        let md = ((start_pos.0 - cheat_pos.0).abs() + (start_pos.1 - cheat_pos.1).abs()) as usize;
+        start_len + md + end_len
+    }
+
+    fn cheat_paths_for_pos(
+        &self,
+        visited: &HashMap<Index, usize>,
+        max_len: usize,
+        pos: Index,
+        time: i64,
+        cheat_map: &mut HashMap<usize, usize>,
+    ) {
+        let cheats = self.cheats_for_pos(pos, time);
+        cheats.iter().for_each(|&cheat_pos| {
+            let total = self.adv_cheat_length(visited, max_len, pos, cheat_pos);
+            if max_len >= total {
+                let savings = max_len - total;
+                *cheat_map.entry(savings).or_default() += 1;
+            }
+        })
+    }
+
+    fn find_num_adv_cheat_paths(
+        &self,
+        visited: &HashMap<Index, usize>,
+        time: i64,
+        min_reduction: usize,
+    ) -> usize {
+        let max_len = *visited.get(&self.end).unwrap();
+        let mut cheat_map: HashMap<usize, usize> = HashMap::new();
+        visited.iter().for_each(|(&pos, _)| {
+            self.cheat_paths_for_pos(visited, max_len, pos, time, &mut cheat_map);
+        });
+
+        cheat_map
+            .iter()
+            .filter(|x| *x.0 >= min_reduction)
+            .map(|x| x.1)
+            .sum()
+    }
+
     fn removable_walls(&self, visited: &HashMap<Index, usize>) -> Vec<(Index, Dir4)> {
         let mut walls = HashSet::<(Index, Dir4)>::new();
         visited.keys().for_each(|&pos| {
@@ -85,35 +156,10 @@ impl Maze {
         let max_path_len = *visited.get(&self.end).unwrap();
         let walls = self.removable_walls(&visited);
         let min_len = max_path_len - min_reduction;
-        let mut map = HashMap::<usize, usize>::new();
         let num_cheats = walls
             .iter()
-            .filter(|&wall| {
-                let cheat_len = self.cheat_length(max_path_len, &visited, *wall);
-                if cheat_len <= max_path_len {
-                    *map.entry(max_path_len - cheat_len).or_default() += 1;
-                }
-                cheat_len <= min_len
-            })
+            .filter(|&wall| self.cheat_length(max_path_len, &visited, *wall) <= min_len)
             .count();
-
-        let mut check = map.iter().collect::<Vec<_>>();
-        check.sort_by(|a, b| {
-            if a.1 == b.1 {
-                a.0.cmp(&b.0)
-            } else {
-                a.1.cmp(&b.1)
-            }
-        });
-        println!("{:?}", check);
-        check.sort_by(|a, b| {
-            if a.0 == b.0 {
-                a.1.cmp(&b.1)
-            } else {
-                a.0.cmp(&b.0)
-            }
-        });
-        println!("{:?}\n", check);
 
         num_cheats
     }
@@ -129,8 +175,10 @@ pub fn part1(input: &str) -> usize {
     maze.find_num_cheat_paths(&visited, 100)
 }
 
-pub fn part2(input: &str) -> String {
-    input.to_string()
+pub fn part2(input: &str) -> usize {
+    let maze = Maze::parse(input).unwrap();
+    let visited = maze.shortest_path();
+    maze.find_num_adv_cheat_paths(&visited, 20, 100)
 }
 
 #[cfg(test)]
@@ -139,9 +187,27 @@ mod tests {
     use super::*;
 
     pub const TEST_INPUT: &str = include_str!("data/input_example");
-    pub const TEST_ANSWER: usize = 3;
-    pub const TEST_INPUT_2: &str = TEST_INPUT;
-    pub const TEST_ANSWER_2: &str = "part2";
+
+    #[test]
+    fn test_cheats_for_pos() {
+        let maze = Maze::parse(TEST_INPUT).unwrap();
+        let test_pos = vec![
+            Index(1, 3),
+            Index(1, 2),
+            Index(3, 3),
+            Index(3, 2),
+            Index(3, 1),
+        ];
+
+        for pos in test_pos {
+            let cheats = maze.cheats_for_pos(pos, 6);
+            cheats.iter().for_each(|cheat_pos| {
+                let md = (cheat_pos.0 - pos.0).abs() + (cheat_pos.1 - pos.1).abs();
+                assert!(md <= 6);
+            });
+            println!("{:?} => {:?}", pos, cheats);
+        }
+    }
 
     #[test]
     fn test_shortest_path() {
@@ -193,5 +259,42 @@ mod tests {
         assert_eq!(maze.find_num_cheat_paths(&visited, 6), 16);
         assert_eq!(maze.find_num_cheat_paths(&visited, 4), 30);
         assert_eq!(maze.find_num_cheat_paths(&visited, 2), 44);
+    }
+
+    #[test]
+    fn test_adv_cheat_len() {
+        let maze = Maze::parse(TEST_INPUT).unwrap();
+        let visited = maze.shortest_path();
+        let max_len = *visited.get(&maze.end).unwrap();
+        assert_eq!(
+            maze.adv_cheat_length(&visited, max_len, Index(1, 3), Index(3, 7)),
+            max_len - 76
+        );
+    }
+
+    #[test]
+    fn test_cheat_paths_for_pos() {
+        let maze = Maze::parse(TEST_INPUT).unwrap();
+        let visited = maze.shortest_path();
+        let max_len = *visited.get(&maze.end).unwrap();
+        let test_pos = vec![
+            Index(1, 3),
+            Index(1, 2),
+            Index(3, 3),
+            Index(3, 2),
+            Index(3, 1),
+        ];
+        test_pos.iter().for_each(|pos| {
+            let mut cheat_map: HashMap<usize, usize> = HashMap::new();
+            maze.cheat_paths_for_pos(&visited, max_len, *pos, 6, &mut cheat_map);
+            println!("{:?} => {:?}", pos, cheat_map);
+        });
+    }
+
+    #[test]
+    fn test_find_num_adv_cheat_paths() {
+        let maze = Maze::parse(TEST_INPUT).unwrap();
+        let visited = maze.shortest_path();
+        assert_eq!(maze.find_num_adv_cheat_paths(&visited, 6, 76), 3);
     }
 }
