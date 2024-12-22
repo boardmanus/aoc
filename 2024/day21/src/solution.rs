5475d7357fc7ed53b2a32e980d44aa14ed497ef1
Craig <boardmanzuz@gmail.com>
2024-12-22 16:19:49 +1300

    Crappy 21/2

--- 2024/day21/src/solution.rs
+++ 2024/day21/src/solution.rs
@@ -6,6 +6,7 @@ use std::{
 use aoc_utils::{
     dir::{Dir, Dir4},
     grid::{Grid, GridPos},
+    str::AocStr,
 };
 
 static KEYPAD: &str = "789\n456\n123\n#0A";
@@ -41,10 +42,11 @@ impl<'a> Controls<'a> {
         }
     }
 
-    fn key_shortest_paths(&self, pad: &Grid<char>, start: &GridPos, c: char) -> Vec<String> {
+    fn key_shortest_paths(&self, pad: &Grid<char>, start_c: char, c: char) -> Vec<String> {
+        let start = pad.find_pos(start_c).unwrap();
         let end = pad.find_pos(c).unwrap();
         let mut visited: HashMap<GridPos, usize> = HashMap::new();
-        let mut possies: VecDeque<(GridPos, String)> = VecDeque::from([(*start, String::new())]);
+        let mut possies: VecDeque<(GridPos, String)> = VecDeque::from([(start, String::new())]);
         let mut paths: Vec<String> = Vec::new();
         let mut shortest = usize::MAX;
         while let Some((pos, path)) = possies.pop_front() {
@@ -79,12 +81,12 @@ impl<'a> Controls<'a> {
     }
 
     fn keypad_shortest_segments(&self, pad: &Grid<char>, code: &str) -> Vec<Vec<String>> {
-        let mut next_start_pos = pad.find_pos('A').unwrap();
+        let mut next_start_pos = 'A';
         code.chars()
             .map(|c| {
                 let start_pos = next_start_pos;
-                next_start_pos = pad.find_pos(c).unwrap();
-                self.key_shortest_paths(pad, &start_pos, c)
+                next_start_pos = c;
+                self.key_shortest_paths(pad, start_pos, c)
             })
             .collect::<Vec<_>>()
     }
@@ -110,36 +112,29 @@ impl<'a> Controls<'a> {
         paths
     }
 
-    fn keypad_3rd_order_shortest_path(&self, code: &str) -> Vec<String> {
-        let paths = self.keypad_shortest_paths(&self.keypad, code);
-        let mut paths_2nd = Vec::<String>::new();
-        let mut shortest = usize::MAX;
-        paths.into_iter().for_each(|p| {
-            let mut paths2 = self.keypad_shortest_paths(&self.dirpad, &p);
-            if paths2[0].len() < shortest {
-                shortest = paths2[0].len();
-                paths_2nd.append(&mut paths2);
-            } else if paths2[0].len() == shortest {
-                paths_2nd.append(&mut paths2);
-            }
-        });
-        let mut paths_3rd = Vec::<String>::new();
-        let mut shortest = usize::MAX;
-        paths_2nd.into_iter().for_each(|p| {
-            let mut paths3 = self.keypad_shortest_paths(&self.dirpad, &p);
-            if paths3[0].len() < shortest {
-                shortest = paths3[0].len();
-                paths_3rd = paths3;
-            } else if paths3[0].len() == shortest {
-                paths_3rd.append(&mut paths3);
-            }
-        });
+    fn keypad_nth_order_shortest_path(&self, code: &str, num_robots: usize) -> Vec<String> {
+        let mut paths = self.keypad_shortest_paths(&self.keypad, code);
+        for _ in 0..num_robots {
+            let mut paths_2nd = Vec::<String>::new();
+            let mut shortest = usize::MAX;
+            paths.into_iter().for_each(|p| {
+                let mut paths2 = self.keypad_shortest_paths(&self.dirpad, &p);
+                if paths2[0].len() < shortest {
+                    shortest = paths2[0].len();
+                    paths_2nd = paths2;
+                } else if paths2[0].len() == shortest {
+                    paths_2nd.append(&mut paths2);
+                }
+            });
 
-        paths_3rd
+            paths = paths_2nd;
+        }
+
+        paths
     }
 
-    fn sequence_complexity(&self, code: &str) -> usize {
-        let paths = self.keypad_3rd_order_shortest_path(code);
+    fn sequence_complexity_old(&self, code: &str, num_robots: usize) -> usize {
+        let paths = self.keypad_nth_order_shortest_path(code, num_robots);
         let len = paths[0].len();
         let code_num = String::from(code.strip_suffix("A").unwrap())
             .parse::<usize>()
@@ -148,18 +143,93 @@ impl<'a> Controls<'a> {
         len * code_num
     }
 
-    fn total_complexity(&self) -> usize {
-        self.codes.iter().map(|c| self.sequence_complexity(c)).sum()
+    fn shortest_path_r(
+        &self,
+        code: &str,
+        num_robots: usize,
+        cache: &mut HashMap<(String, usize), String>,
+    ) -> String {
+        if code == "A" {
+            "A".to_string()
+        } else if let Some(sp) = cache.get(&(code.to_string(), num_robots)) {
+            if num_robots == 1 {
+                sp.clone()
+            } else {
+                self.shortest_path_r(&sp.clone(), num_robots - 1, cache)
+            }
+        } else {
+            let path = if code.len() == 2 {
+                self.key_shortest_paths(&self.dirpad, code.nth(0), code.nth(1))
+                    .into_iter()
+                    .fold(String::new(), |sp, p| {
+                        if !sp.is_empty() && sp.len() <= p.len() {
+                            sp
+                        } else {
+                            p
+                        }
+                    })
+            } else {
+                (0..code.len() - 1)
+                    .map(|i| self.shortest_path_r(&code[i..=i + 1], num_robots, cache))
+                    .collect::<String>()
+            };
+            //assert_eq!(path.first(), Some('A'));
+            if num_robots == 1 {
+                path
+            } else {
+                assert!(path.len() > 1 || path == "A");
+                cache.insert((code.to_string(), num_robots - 1), path.clone());
+                self.shortest_path_r(code, num_robots - 1, cache)
+            }
+        }
+    }
+
+    fn shortest_path(&self, code: &str, num_robots: usize) -> String {
+        let mut cache: HashMap<(String, usize), String> = HashMap::new();
+        self.keypad_shortest_paths(&self.keypad, code)
+            .into_iter()
+            .map(|path| "A".to_string() + &path)
+            .map(|dir_code| self.shortest_path_r(&dir_code, num_robots, &mut cache))
+            .fold(String::new(), |sp, p| {
+                if !sp.is_empty() && sp.len() <= p.len() {
+                    sp
+                } else {
+                    p
+                }
+            })
+    }
+
+    fn sequence_complexity(&self, code: &str, shortest_path: &str) -> usize {
+        let len = shortest_path.len();
+        let code_num = String::from(code.strip_suffix("A").unwrap())
+            .parse::<usize>()
+            .unwrap();
+
+        len * code_num
+    }
+    fn total_complexity(&self, num_robots: usize) -> usize {
+        self.codes
+            .iter()
+            .map(|&code| self.sequence_complexity(code, &self.shortest_path(code, num_robots)))
+            .sum()
+    }
+
+    fn total_complexity_old(&self, num_robots: usize) -> usize {
+        self.codes
+            .iter()
+            .map(|c| self.sequence_complexity_old(c, num_robots))
+            .sum()
     }
 }
 
 pub fn part1(input: &str) -> usize {
     let controls = Controls::parse(input);
-    controls.total_complexity()
+    controls.total_complexity_old(2)
 }
 
-pub fn part2(input: &str) -> String {
-    input.to_string()
+pub fn part2(input: &str) -> usize {
+    let controls = Controls::parse(input);
+    controls.total_complexity(25)
 }
 
 #[cfg(test)]
@@ -170,30 +240,23 @@ mod tests {
     use super::*;
 
     pub const TEST_INPUT: &str = include_str!("data/input_example");
-    pub const TEST_ANSWER: &str = "part1";
-    pub const TEST_INPUT_2: &str = TEST_INPUT;
-    pub const TEST_ANSWER_2: &str = "part2";
 
     #[test]
     fn test_key_shorest_paths() {
         let controls = Controls::parse(TEST_INPUT);
-        let start = controls.keypad.find_pos('A').unwrap();
-        let sp = controls.key_shortest_paths(&controls.keypad, &start, '0');
+        let sp = controls.key_shortest_paths(&controls.keypad, 'A', '0');
         assert_eq!(sp, vec!["<A".to_string()]);
 
-        let start = controls.keypad.find_pos('0').unwrap();
-        let sp = controls.key_shortest_paths(&controls.keypad, &start, '2');
+        let sp = controls.key_shortest_paths(&controls.keypad, '0', '2');
         assert_eq!(sp, vec!["^A".to_string()]);
 
-        let start = controls.keypad.find_pos('2').unwrap();
-        let mut sp = controls.key_shortest_paths(&controls.keypad, &start, '9');
+        let mut sp = controls.key_shortest_paths(&controls.keypad, '2', '9');
         sp.sort();
         let mut res = vec![">^^A".to_string(), "^>^A".to_string(), "^^>A".to_string()];
         res.sort();
         assert_eq!(sp, res);
 
-        let start = controls.keypad.find_pos('9').unwrap();
-        let sp = controls.key_shortest_paths(&controls.keypad, &start, 'A');
+        let sp = controls.key_shortest_paths(&controls.keypad, '9', 'A');
         assert_eq!(sp, vec!["vvvA".to_string()]);
     }
 
@@ -224,9 +287,22 @@ mod tests {
     }
 
     #[test]
+    fn test_shortest_path() {
+        let controls = Controls::parse(TEST_INPUT);
+        let path = controls.shortest_path("029A", 2);
+        println!("shortest-path: {:?}", path);
+        assert_eq!(path.len(), "v<<A>>^A<A>AvA<^AA>A<vAAA>^A".len());
+
+        //for num_robots in 2..=25 {
+        let path = controls.shortest_path("029A", 10); //num_robots);
+        println!("{}: {:?}", path.len(), path);
+        //}
+    }
+
+    #[test]
     fn test_sequence_complexity() {
         let controls = Controls::parse(TEST_INPUT);
-        let paths = controls.keypad_3rd_order_shortest_path("029A");
+        let paths = controls.keypad_nth_order_shortest_path("029A", 2);
         let h = paths.iter().collect::<HashSet<_>>();
         assert_eq!(h.len(), paths.len());
     }
@@ -234,7 +310,7 @@ mod tests {
     #[test]
     fn test_3rd_keypad_shortest_paths() {
         let controls = Controls::parse(TEST_INPUT);
-        let paths = controls.keypad_3rd_order_shortest_path("029A");
+        let paths = controls.keypad_nth_order_shortest_path("029A", 2);
 
         assert!(paths.contains(
             &"<vA<AA>>^AvAA<^A>A<v<A>>^AvA^A<vA>^A<v<A>^A>AAvA^A<v<A>A>^AAAvA<^A>A".to_string()
