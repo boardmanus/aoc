41f4f8e00f07d1e8a933f4c8875de029b8456969
Craig <boardmanzuz@gmail.com>
2024-12-22 17:40:55 +1300

    Add a string nth helper

--- aoc-utils/src/str.rs
+++ aoc-utils/src/str.rs
@@ -1,20 +1,25 @@
 pub trait AocStr {
-  fn first(&self) -> Option<char>;
+    fn first(&self) -> Option<char>;
+    fn nth(&self, n: usize) -> char;
 }
 
 impl AocStr for str {
-  fn first(&self) -> Option<char> {
-    self.chars().next()
-  }
+    fn first(&self) -> Option<char> {
+        self.chars().next()
+    }
+
+    fn nth(&self, n: usize) -> char {
+        self.chars().nth(n).unwrap()
+    }
 }
 
 #[cfg(test)]
 mod tests {
-  use super::*;
+    use super::*;
 
-  #[test]
-  fn test_first() {
-    assert_eq!("abc".first(), Some('a'));
-    assert_eq!("".first(), None);
-  }
+    #[test]
+    fn test_first() {
+        assert_eq!("abc".first(), Some('a'));
+        assert_eq!("".first(), None);
+    }
 }
