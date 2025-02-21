25a7b26c061b8868bae8ac9035649a67d2ce2b0b
Craig <boardmanzuz@gmail.com>
2025-02-21 21:59:06 +1300

    Clean up day12

--- 2024/day12/src/solution.rs
+++ 2024/day12/src/solution.rs
@@ -1,34 +1,36 @@
 #![allow(dead_code)]
 
-use std::collections::HashSet;
+use std::{collections::HashSet, rc::Rc};
 
 use aoc_utils::{
-    dir::{Dir, Dir4},
+    dir::Dir4,
     grof,
     grud::{Grid, GridPos},
 };
 
+type PlotOp = fn(plot: &Plot) -> usize;
+
 struct Plot {
-    vege: char,
+    grid: Rc<Grid<char, Dir4>>,
     locations: Vec<GridPos>,
 }
 
 impl Plot {
-    fn new(garden: &Garden, index: GridPos) -> Plot {
-        let vege = garden.0.at(&index).unwrap();
-        let mut locations = garden.filter_connected_dfs(index);
+    fn new(grid: Rc<Grid<char, Dir4>>, index: GridPos) -> Plot {
+        let mut locations: Vec<GridPos> = Vec::new();
+        grof::algorithms::dfs(grid.as_ref(), index, |n| locations.push(*n));
         locations.sort();
-        Plot { vege, locations }
+        Plot { grid, locations }
     }
 
-    fn perimeter(&self, garden: &Garden) -> usize {
+    fn perimeter(&self) -> usize {
         self.locations
             .iter()
-            .map(|&i| 4 - garden.0.neighbours(i).count())
+            .map(|&i| 4 - self.grid.neighbours(i).count())
             .sum()
     }
 
-    fn corners(&self, garden: &Garden) -> usize {
+    fn corners(&self) -> usize {
         self.locations
             .iter()
             .map(|&pos| {
@@ -43,7 +45,7 @@ impl Plot {
                 // Get the vege in each corner pos
                 // NE => 1 3 SW => 2 0
                 //       0 2       3 1
-                .map(|v| [pos, pos + v.0, pos + v.1, pos + v.0 + v.1].map(|p| garden.0.at(&p)))
+                .map(|v| [pos, pos + v.0, pos + v.1, pos + v.0 + v.1].map(|p| self.grid.at(&p)))
                 .filter(|p| {
                     // keep if both side positions don't match, or bot side positions do match, and diag doesn't
                     // y .  ||  x y  => corner
@@ -55,112 +57,65 @@ impl Plot {
             .sum()
     }
 
-    fn sides2(&self, garden: &Garden) -> usize {
+    fn sides(&self) -> usize {
         // number of sides == number of corners
-        self.corners(garden)
+        self.corners()
     }
 
-    fn sides(&self, garden: &Garden) -> usize {
-        let mut all_edges: HashSet<(GridPos, Dir4)> =
-            self.locations
-                .iter()
-                .fold(HashSet::new(), |mut col, &index| {
-                    garden.edge_dirs(index, self.vege, &mut col);
-                    col
-                });
-        let mut num_sides = 0usize;
-
-        while let Some(&(start_pos, start_dir)) = all_edges.iter().next() {
-            let mut check_dir = start_dir;
-            let mut travel_dir = start_dir.rotate_cw();
-            let mut next_pos = start_pos + travel_dir;
-            all_edges.remove(&(start_pos, start_dir));
-            while start_pos != next_pos || start_dir != check_dir {
-                if garden.0.at(&next_pos) != Some(self.vege) {
-                    next_pos = next_pos - travel_dir.to_vec2d();
-                    travel_dir = travel_dir.rotate_cw();
-                    check_dir = check_dir.rotate_cw();
-                    num_sides += 1;
-                } else if garden.0.at(&(next_pos + check_dir)) == Some(self.vege) {
-                    next_pos = next_pos + check_dir;
-                    travel_dir = travel_dir.rotate_ccw();
-                    check_dir = check_dir.rotate_ccw();
-                    num_sides += 1;
-                } else {
-                    all_edges.remove(&(next_pos, check_dir));
-                    next_pos = next_pos + travel_dir;
-                }
-            }
-        }
-        num_sides
+    fn fence_cost(&self) -> usize {
+        self.perimeter() * self.locations.len()
     }
 
-    fn fence_cost(&self, garden: &Garden, discount: bool) -> usize {
-        let value = if discount {
-            self.sides2(garden)
-        } else {
-            self.perimeter(garden)
-        };
-
-        value * self.locations.len()
+    fn discounted_fence_cost(&self) -> usize {
+        self.sides() * self.locations.len()
     }
 }
 
-struct Garden(Grid<char, Dir4>);
+struct Garden {
+    grid: Rc<Grid<char, Dir4>>,
+    plots: Vec<Plot>,
+}
 
 impl Garden {
     fn parse(input: &str) -> Garden {
-        Garden(Grid::<char, Dir4>::parse_walkable(input, |g, from, to| {
+        let grid = Rc::new(Grid::<char, Dir4>::parse_walkable(input, |g, from, to| {
             g.at(from) == g.at(to)
-        }))
-    }
-
-    fn edge_dirs(&self, index: GridPos, vege: char, edges: &mut HashSet<(GridPos, Dir4)>) {
-        Dir4::cw().for_each(|dir| {
-            if self.0.at(&(index + dir)) != Some(vege) {
-                edges.insert((index, dir));
-            }
-        });
-    }
-
-    fn filter_connected_dfs(&self, index: GridPos) -> Vec<GridPos> {
-        let mut locations: Vec<GridPos> = Vec::new();
-        grof::algorithms::dfs(&self.0, index, |n| locations.push(*n));
-        locations
-    }
-
-    fn create_plots(&self) -> Vec<Plot> {
+        }));
         let mut used_locations: HashSet<GridPos> = HashSet::new();
-
-        self.0.iter_pos().fold(Vec::<Plot>::new(), |mut acc, v| {
+        let plots = grid.iter_pos().fold(Vec::<Plot>::new(), |mut acc, v| {
             if !used_locations.contains(&v) {
-                let plot = Plot::new(self, v);
+                let plot = Plot::new(grid.clone(), v);
                 plot.locations.iter().for_each(|&l| {
                     used_locations.insert(l);
                 });
                 acc.push(plot);
             }
             acc
-        })
+        });
+        Garden { grid, plots }
     }
 
-    fn fence_cost(&self, discount: bool) -> usize {
-        let plots = self.create_plots();
-        plots
-            .iter()
-            .map(|plot| plot.fence_cost(self, discount))
-            .sum()
+    fn plot_sum(&self, op: PlotOp) -> usize {
+        self.plots.iter().map(op).sum()
+    }
+
+    fn fence_cost(&self) -> usize {
+        self.plot_sum(|plot| plot.fence_cost())
+    }
+
+    fn discounted_fence_cost(&self) -> usize {
+        self.plot_sum(|plot| plot.discounted_fence_cost())
     }
 }
 
 pub fn part1(input: &str) -> usize {
-    let garden = Garden::parse(input);
-    garden.fence_cost(false)
+    let garden: Garden = Garden::parse(input);
+    garden.fence_cost()
 }
 
 pub fn part2(input: &str) -> usize {
     let garden = Garden::parse(input);
-    garden.fence_cost(true)
+    garden.discounted_fence_cost()
 }
 
 #[cfg(test)]
@@ -186,7 +141,7 @@ mod tests {
     #[test]
     fn test_plot_new() {
         let garden = Garden::parse(TEST_INPUT);
-        let plot = Plot::new(&garden, GridPos::new(0, 0));
+        let plot = Plot::new(garden.grid, GridPos::new(0, 0));
         let v = plot.locations.clone();
         let v2 = vec![
             GridPos::new(0, 0),
@@ -203,14 +158,13 @@ mod tests {
             GridPos::new(2, 3),
         ];
         assert_eq!(v, v2);
-        assert_eq!(garden.0.at(&GridPos::new(0, 0)).unwrap(), plot.vege);
     }
 
     #[test]
     fn test_plot_sides() {
         let garden = Garden::parse(TEST_INPUT_P2_1);
-        let plot = Plot::new(&garden, GridPos::new(2, 1));
-        assert_eq!(plot.sides(&garden), 8);
+        let plot = Plot::new(garden.grid, GridPos::new(2, 1));
+        assert_eq!(plot.sides(), 8);
     }
     #[test]
     fn test_part1_2() {
