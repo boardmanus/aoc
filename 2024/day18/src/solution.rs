49c4372570e6ac5a9874980fc90f035ca7097b4c
Craig <boardmanzuz@gmail.com>
2025-11-16 19:04:34 +1300

    Add binary search for day18

--- 2024/day18/src/solution.rs
+++ 2024/day18/src/solution.rs
@@ -1,12 +1,6 @@
-use std::{
-    collections::{hash_map::Entry, HashMap, VecDeque},
-    hash::Hash,
-};
-
 use aoc_utils::{
     dir::Dir4,
-    grif::algorithms::shortest_path_djikstra,
-    grif::iterators::BfsIter,
+    grif::Graph,
     grud::{Grid, GridPos},
 };
 
@@ -28,49 +22,67 @@ fn create_grid(blocks: &[GridPos], width: usize, height: usize) -> Grid<char, Di
     grid
 }
 
-fn shortest_path2(grid: &Grid<char, Dir4>, start: GridPos, end: GridPos) -> Option<usize> {
-    BfsIter::new(grid, start).find_map(|(pos, level)| if pos == end { Some(level) } else { None })
-}
-
 fn shortest_path(grid: &Grid<char, Dir4>, start: GridPos, end: GridPos) -> Option<usize> {
-    let mut visited: HashMap<GridPos, usize> = HashMap::new();
-    let mut nexties: VecDeque<(GridPos, usize)> = VecDeque::from([(start, 0)]);
-    while let Some((pos, dist)) = nexties.pop_front() {
-        if pos == end {
-            return Some(dist);
-        } else {
-            if let Entry::Vacant(e) = visited.entry(pos) {
-                e.insert(dist);
-                nexties.extend(grid.neighbours(pos).map(|pos| (pos, dist + 1)));
-            }
-        }
-    }
-
-    None
+    grid.bfs(start)
+        .find_map(|(pos, level)| if pos == end { Some(level) } else { None })
 }
 
+#[allow(dead_code)]
 fn first_blocker(grid: &mut Grid<char, Dir4>, blocks: &[GridPos]) -> Option<GridPos> {
     let start = GridPos::new(0, 0);
     let end = GridPos::new(grid.width() as i64 - 1, grid.height() as i64 - 1);
+
     for &block in blocks {
         grid.set(&block, '#');
-        if shortest_path2(grid, start, end).is_none() {
+        if shortest_path(grid, start, end).is_none() {
             return Some(block);
         }
     }
     None
 }
 
+fn first_blocker_binary_search(grid: &mut Grid<char, Dir4>, blocks: &[GridPos]) -> Option<GridPos> {
+    let start = GridPos::new(0, 0);
+    let end = GridPos::new(grid.width() as i64 - 1, grid.height() as i64 - 1);
+
+    let mut low = 0;
+    let mut high = blocks.len();
+    let mut mid = (low + high) / 2;
+
+    while low < mid {
+        for &block in &blocks[low..mid] {
+            grid.set(&block, '#');
+        }
+
+        for &block in &blocks[mid..high] {
+            grid.set(&block, '.');
+        }
+
+        if shortest_path(grid, start, end).is_some() {
+            low = mid;
+        } else {
+            high = mid;
+        }
+        mid = (low + high) / 2;
+    }
+
+    if low < blocks.len() {
+        Some(blocks[low])
+    } else {
+        None
+    }
+}
+
 pub fn part1(input: &str) -> usize {
     let blocks = parse_input(input);
     let grid = create_grid(&blocks[0..1024], 71, 71);
-    shortest_path2(&grid, GridPos::new(0, 0), GridPos::new(70, 70)).unwrap()
+    shortest_path(&grid, GridPos::new(0, 0), GridPos::new(70, 70)).unwrap()
 }
 
 pub fn part2(input: &str) -> String {
     let blocks = parse_input(input);
     let mut grid = create_grid(&blocks[0..1024], 71, 71);
-    let blocker = first_blocker(&mut grid, &blocks[1024..]).unwrap();
+    let blocker = first_blocker_binary_search(&mut grid, &blocks[1024..]).unwrap();
     format!("{},{}", blocker.x, blocker.y)
 }
 
@@ -105,7 +117,7 @@ mod tests {
     fn test_part2() {
         let blocks = parse_input(TEST_INPUT_2);
         let mut grid = create_grid(&blocks[0..12], 7, 7);
-        let blocker = first_blocker(&mut grid, &blocks[12..]).unwrap();
+        let blocker = first_blocker_binary_search(&mut grid, &blocks[12..]).unwrap();
         assert_eq!(blocker, TEST_ANSWER_2)
     }
 }
