2e2e44ba8239973b6b776113034bf40ac8ef9a48
Craig <boardmanzuz@gmail.com>
2025-11-29 18:42:37 +1300

    Add a dryrun option to download

--- download.sh
+++ download.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 set -e
 
+if [[ "$1" == "-n" ]]; then
+  readonly DRYRUN="echo "
+  shift
+else
+  readonly DRYRUN=""
+fi
+
 path="${PWD}"
 if [[ -z "${1}" ]]; then
   readonly day="$(basename ${PWD})"
@@ -16,7 +23,7 @@ if (( $day_num < 32 )) \
   && [[ "$day" =~ ^day[0-9]+$ ]] \
   && [[ "$day_num" =~ ^[0-9]+$ ]]  \
   &&  [[ "$year" =~ ^[0-9]+$  ]]; then
-  echo "Generating stuff for day=$day, day_num=$day_nu, year=$year..."
+  echo "Generating stuff for day=$day, day_num=$day_num, year=$year..."
 else
   echo "Running from the wrong directory: pwd=$PWD, day=$day, day_num=$day_num, year=$year"
   exit 1
@@ -26,14 +33,14 @@ readonly base_path="$(realpath $(dirname $0))"
 readonly project_path="$base_path/$year/$day"
 
 if [[ ! -d ${project_path} ]]; then
-  cargo generate \
+  ${DRYRUN} cargo generate \
     --path $base_path/template \
     --name ${day} \
     --define aoc_year=${year} \
     --define aoc_day=${day_num}
 fi
 
-aoc download \
+${DRYRUN}aoc download \
   --year ${year} \
   --day ${day_num} \
   --input-file $project_path/src/data/input \
